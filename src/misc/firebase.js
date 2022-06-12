bb0b3779258eada6580f39ffbbd08e8ec902c184
MdAzeen <azeenmd4@gmail.com>
2022-06-13 01:27:53 +0530

    Initialised firebased project

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyBvkRMqR5skCzB1Zmg_TJpS42HXMweH7zw",
+    authDomain: "chat-web-app-f7736.firebaseapp.com",
+    databaseURL: "https://chat-web-app-f7736-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "chat-web-app-f7736",
+    storageBucket: "chat-web-app-f7736.appspot.com",
+    messagingSenderId: "434257146201",
+    appId: "1:434257146201:web:fb576846d7d301665f9c48"
+  };
+
+  const app=firebase.initializeApp(config);
+  
