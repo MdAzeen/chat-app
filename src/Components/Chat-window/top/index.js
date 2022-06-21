b459ee62c9bb9c39a366fb755e2ecdfaf8a552f2
MdAzeen <azeenmd4@gmail.com>
2022-06-22 01:03:09 +0530

    Started to build chat top and created room description

--- src/Components/Chat-window/top/index.js
+++ src/Components/Chat-window/top/index.js
@@ -1,11 +1,39 @@
-import React, {memo} from 'react'
-import { useCurrentRoom } from '../../../context/current-room.context'
+import React, { memo } from 'react';
+import { Link } from 'react-router-dom';
+import { ButtonToolbar, Icon } from 'rsuite';
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { useMediaQuery } from '../../../misc/custom-hook';
+import RoomInfoBtnModal from './RoomInfoBtnModal';
 
 function Top() {
-  const name=useCurrentRoom(a=>a.name)
+  const name = useCurrentRoom(a => a.name);
+  const isMobile = useMediaQuery('(max-width: 992px)');
+
   return (
-    <div>{name}</div>
-  )
+    <div>
+      <div className="d-flex justify-content-between align-items-center">
+        <h4>
+          <Icon
+            componentClass={Link}
+            to="/"
+            icon="arrow-circle-left"
+            size="2x"
+            className={
+              isMobile
+                ? 'd-inline-block p-0 mr-2 text-blue link-unstyled'
+                : 'd-none'
+            }
+          />
+          <span className='text-disappear'>{name}</span>
+        </h4>
+        <ButtonToolbar className='ws-nowrap'>to do</ButtonToolbar>
+      </div>
+      <div className='d-flex justify-content-between align-items-center'>
+          <span>todo</span>
+          <RoomInfoBtnModal />
+      </div>
+    </div>
+  );
 }
 
 export default memo(Top);
