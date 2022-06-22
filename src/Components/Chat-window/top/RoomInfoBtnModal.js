db52e077d87431e97483ec4e272c9e09a7900c0a
MdAzeen <azeenmd4@gmail.com>
2022-06-23 01:47:56 +0530

    created chat bottom and send message to database

--- src/Components/Chat-window/top/RoomInfoBtnModal.js
+++ src/Components/Chat-window/top/RoomInfoBtnModal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { memo } from 'react'
 import { Button, Modal } from 'rsuite';
 import { useCurrentRoom } from '../../../context/current-room.context'
 import { useModalState } from '../../../misc/custom-hook';
@@ -28,4 +28,4 @@ function RoomInfoBtnModal() {
   )
 }
 
-export default RoomInfoBtnModal
+export default memo(RoomInfoBtnModal)
